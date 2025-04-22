d41beee417497e38e1b4cffe2201370729b68bd1
viniciusfinger <fingervinicius@icloud.com>
2025-04-22 20:22:21 -0300

    Add information about tattoo artists, studio and collect more data

--- backend/agent/attendance_agent.py
+++ backend/agent/attendance_agent.py
@@ -8,23 +8,31 @@ def attendance_agent(state: State):
     """Agent responsible for attending to the studio's clients"""
     llm = ChatOpenAI(model="gpt-4o", temperature=0.7)
     tools = [tool_get_client_info]
-    
-    #TODO: Adicionar guardrails + prevenção de prompt injection
-    #TODO: Adicionar mais informações sobre o estúdio e tatuadores
-    
+
     prompt_template = ChatPromptTemplate.from_messages(
         [
-            ("system", """     
-                Você é um funcionário do estúdio Passione Tattoo e atende os clientes em seu primeiro contato para agendar uma tatuagem.
-        
+            ("system", """
+                Você é um funcionário do estúdio Passione Tattoo Studio e atende os clientes em seu primeiro contato para agendar uma tatuagem.
+                
+                O estúdio fica localizado no centro de Canoas, Rio Grande do Sul. Na rua Rua dr barcelos, 2451, sala 501.
+                O telefone de contato é (51)98989-7802
+                
+                Os tatuadores disponíveis são:
+                    - Jean Szimanski, especialista em Geek, blackwork e Fullcolor. Instagram: @Jeanzink_
+                    - Maria Quelipe, especialista em Geek, whipshading e delicadas. Instagram: @Marye.ink
+                    - Gustavo Campos, especialista em Geek, whipshading e dark. Instagram: @guszink
+                    
+                Tire possíveis dúvidas sobre os tatuadores e suas especialidades, além de explicar os estilos de tatuagem, caso o usuário solicite.
+                
                 Para isso, você deve utilizar as seguinte ferramenta disponíveis para coletar as informações necessárias para agendar a tatuagem:
                 
-                1. **tool_get_client_info**  :
+                1. **tool_get_client_info**:
                     Utilize esta ferramenta para coletar informações básicas do cliente, como nome, telefone e dados sobre a arte que o cliente quer tatuar.
                     - **Argumentos:**  
                         - `client_name`: (Opcional): Nome do cliente.
                         - `client_phone`: (Opcional): O telefone do cliente.
                         - `client_has_art_ready`: (Opcional): Se a arte que o cliente quer tatuar está pronta ou precisará ser criada.
+                        - `tattoo_artist`: (Opcional): Nome do tatuador que o cliente quer agendar.
                         
                         - **Comportamento esperado:**  
                         Se solicitado, a ferramenta pode retornar mensagens para pedir mais informações ou fornecer as informações já obtidas.
@@ -33,7 +41,8 @@ def attendance_agent(state: State):
                         
                         Seja claro, legal e gentil. Use uma linguagem informal, jovial e descontraída com o usuário.
                     
-                        Responda na língua portuguesa do Brasil.
+                        Responda na língua portuguesa do Brasil, não use markdown.
+                        Qualquer pergunta que não seja sobre o estúdio, tatuadores ou o contexto de tattoo, deve ser respondida com "Desculpe, não posso te ajudar com isso, estou aqui para te atender sobre o estúdio e os tatuadores."
             """),
             ("placeholder", "{messages}")
         ]
@@ -48,6 +57,7 @@ def tool_get_client_info(
     client_name: Optional[str] = None,
     client_phone: Optional[str] = None,
     client_has_art_ready: Optional[bool] = None,
+    tattoo_artist: Optional[str] = None,
 ) -> str:
     """
     Utilize esta ferramenta para obter informações sobre o cliente.
@@ -56,6 +66,7 @@ def tool_get_client_info(
         client_name: Optional[str] - Nome do cliente
         client_phone: Optional[str] - Telefone do cliente
         client_has_art_ready: Optional[bool] - Se a arte que o cliente quer tatuar está pronta ou precisará ser criada
+        tattoo_artist: Optional[str] - Nome do tatuador que o cliente quer agendar
     Returns:
         Retorna as informações do cliente agrupadas.
     """
@@ -66,6 +77,8 @@ def tool_get_client_info(
         return "Por favor, informe qual é o seu telefone para contato"
     if client_has_art_ready == None:
         return "Por favor, informe se a arte que o cliente quer tatuar está pronta ou precisará ser criada"
+    if tattoo_artist == None:
+        return "Por favor, informe qual é o tatuador que o cliente quer agendar"
 
     return f"""
     Os dados foram coletados com sucesso:
@@ -73,6 +86,7 @@ def tool_get_client_info(
     O nome do cliente é: {client_name}
     O telefone do cliente é: {client_phone}
     A arte que o cliente quer tatuar está pronta? {client_has_art_ready}
+    O tatuador que o cliente quer agendar é: {tattoo_artist}
     """
 
 def tool_persist_client_info(client_info: str):
