49a540ce17a6419306aec74f3c021e452b18d498
Kauã Segabinazzi <101999858+Kauasegabinazzi@users.noreply.github.com>
2025-06-04 20:17:50 -0300

    arrumando as rotas

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,9 +1,12 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { StatusComponent } from './components/status/status.component';
+import { LoginComponent } from './components/login/login.component';
 
 export const routes: Routes = [
     { path: 'home', component: HomeComponent },
     { path: 'status', component: StatusComponent },
+    { path: 'login', component: LoginComponent },
     { path: '', redirectTo: 'home', pathMatch: 'full' },
+    { path: '**', redirectTo: 'home' }
 ];
