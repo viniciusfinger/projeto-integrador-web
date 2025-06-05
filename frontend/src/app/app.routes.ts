f86aa197918bf38e0454917b4152f9094efc6f6f
Kauã Segabinazzi <101999858+Kauasegabinazzi@users.noreply.github.com>
2025-06-04 21:30:33 -0300

    tela nova de chat

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -2,11 +2,13 @@ import { Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { StatusComponent } from './components/status/status.component';
 import { LoginComponent } from './components/login/login.component';
+import { ChatComponent } from './components/chat/chat.component';
 
 export const routes: Routes = [
     { path: 'home', component: HomeComponent },
     { path: 'status', component: StatusComponent },
     { path: 'login', component: LoginComponent },
+    { path: 'chat', component: ChatComponent },
     { path: '', redirectTo: 'home', pathMatch: 'full' },
     { path: '**', redirectTo: 'home' }
 ];
