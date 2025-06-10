357121a81296559015057ca680fd8a5535611110
Vinicius Finger <fingervinicius@icloud.com>
2025-06-10 19:38:35 -0300

    Bootstrap uvicorn

--- backend/main.py
+++ backend/main.py
@@ -1,6 +1,16 @@
 from controller import auth_controller
 from fastapi import FastAPI
+import uvicorn
+from model.user import Base
+from database import engine
+
+
+Base.metadata.create_all(bind=engine)
+
 
 app = FastAPI()
 
 app.include_router(auth_controller.router, tags=["auth"])
+
+if __name__ == "__main__":
+    uvicorn.run(app, host="0.0.0.0", port=8000)
