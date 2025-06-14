ceb122785424bbef11614405474b40fdab270e54
Vinicius Finger <fingervinicius@icloud.com>
2025-06-14 19:41:39 -0300

    disable cors

--- backend/main.py
+++ backend/main.py
@@ -1,5 +1,6 @@
 from controller import auth_controller
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 import uvicorn
 from model.user import Base
 from database import engine
@@ -9,6 +10,14 @@ Base.metadata.create_all(bind=engine)
 
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"], 
+    allow_headers=["*"], 
+)
 
 app.include_router(auth_controller.router, tags=["auth"])
 
